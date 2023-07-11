bf32108b9664f372a6933bfaf9deb1ff6b4e9303
Al Ameen <78306701+radioactivein@users.noreply.github.com>
2023-07-11 12:56:51 +0530

    owl

---
+++ js/owl-local.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    info:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        900:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
